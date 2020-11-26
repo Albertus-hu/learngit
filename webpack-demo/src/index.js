23c6f8fd9fb0eaee9eaba22be5d542c1acedfa8d
焱雨 <hu950926@163.com>
2020-11-26 15:16:05 +0800

    add files

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,8 @@
+import _ from 'lodash';
+
 function component() {
   let element = document.createElement('div');
 
-  //Lodash(目前通过一个script脚本引入)
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
